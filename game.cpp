e714a69148d848bf1d5c8ba30cc3b72ac93ea1e8
gusahlg <gusahlg@gmail.com>
2025-05-07 18:44:09 +0100

    I am introducing pointers

--- game.cpp
+++ game.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
-void gameUpdate(bool gameRunning, int snakePositionY, int snakePositionX, char snake, std::string username, int foodPositionY, int foodPositionX, char food, int numberofSnake, int previousY, int previousX){
+#include <algorithm>
+std::string username;
+bool gameRunning = false;
+char snake = '8';
+char food = 'E';
+int numberofSnake = 0;
+int previousY = 1;
+int previousX = 1;
+int foodPositionY = 5;
+int foodPositionX = 5;
+int snakePositionY = 1;
+int snakePositionX = 1;
+int* previouspositionsY[50];
+int* previouspositionsX[50];
+void gameUpdate(){
+    system("cls");
     std::cout << "Hello " << username << "!\n";
     for(int i = 1; i <= 13; i++){
         for(int j = 1; j <= 10; j++){
             if(snakePositionY == i && snakePositionX == j){
-                std::cout << "| " << snake << " ";
+                std::cout << "| " << "X" << " ";
             }
-            if(foodPositionY == i && foodPositionX == j){
+            else if(foodPositionY == i && foodPositionX == j){
                 std::cout << "| " << food << " ";
             }
-            if(previousY == i && previousX == j){
-                    std::cout << "| " << snake << " ";
+            if(std::find(std::begin(previouspositionsY), std::end(previouspositionsY), i) != std::end(previouspositionsY) && std::find(std::begin(previouspositionsX), std::end(previouspositionsX), j) != std::end(previouspositionsX)){
+                std::cout << "| " << snake << " ";
             }
             else{
                 std::cout << "|   ";
@@ -22,17 +37,6 @@ void gameUpdate(bool gameRunning, int snakePositionY, int snakePositionX, char s
     }
 }
 int main(){
-    std::string username;
-    bool gameRunning = false;
-    char snake = '8';
-    char food = 'E';
-    int numberofSnake = 1;
-    int previousY = 0;
-    int previousX = 0;
-    int foodPositionY = 5;
-    int foodPositionX = 5;
-    int snakePositionY = 1;
-    int snakePositionX = 1;
     std::cout << "Snake\n";
     while(username.empty()){
         std::cout << "Enter a username to start playing:\n";
@@ -40,15 +44,17 @@ int main(){
     }
     if(!username.empty()){
         gameRunning = true;
-        gameUpdate(gameRunning, snakePositionY, snakePositionX, snake, username, foodPositionY, foodPositionX, food, numberofSnake, previousY, previousX);
+        gameUpdate();
     }
     while(gameRunning){
         if(_kbhit()){
+            previousY = snakePositionY;
+            previousX = snakePositionX;
             switch(_getch()){
-                case 'w': previousY = snakePositionY; --snakePositionY; gameUpdate(gameRunning, snakePositionY, snakePositionX, snake, username, foodPositionY, foodPositionX, food, numberofSnake, previousY, previousX); break;
-                case 's': previousY = snakePositionY; ++snakePositionY; gameUpdate(gameRunning, snakePositionY, snakePositionX, snake, username, foodPositionY, foodPositionX, food, numberofSnake, previousY, previousX); break;
-                case 'a': previousX = snakePositionX; --snakePositionX; gameUpdate(gameRunning, snakePositionY, snakePositionX, snake, username, foodPositionY, foodPositionX, food, numberofSnake, previousY, previousX); break;
-                case 'd': previousX = snakePositionX; ++snakePositionX; gameUpdate(gameRunning, snakePositionY, snakePositionX, snake, username, foodPositionY, foodPositionX, food, numberofSnake, previousY, previousX); break;
+                case 'w': --snakePositionY; gameUpdate(); break;
+                case 's': ++snakePositionY; gameUpdate(); break;
+                case 'a': --snakePositionX; gameUpdate(); break;
+                case 'd': ++snakePositionX; gameUpdate(); break;
             }
         }
         if(snakePositionY <= 0 || snakePositionY >= 14 || snakePositionX >= 11 || snakePositionX <= 0){
@@ -57,8 +63,11 @@ int main(){
         if(snakePositionX == foodPositionX && snakePositionY == foodPositionY){
             foodPositionY = (rand() % 13) + 1;
             foodPositionX = (rand() % 10) + 1;
-            numberofSnake += 1;
-            gameUpdate(gameRunning, snakePositionY, snakePositionX, snake, username, foodPositionY, foodPositionX, food, numberofSnake, previousY, previousX);
+        //Add so that previous positions are documented. I am stupid the values put into the array are not changing and following the snake.
+            previouspositionsY[numberofSnake] = snakePositionY;
+            previouspositionsX[numberofSnake] = snakePositionX;
+            numberofSnake++;
+            gameUpdate();
         }
         Sleep(250);
     }
